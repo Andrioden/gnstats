8b4c9adeb579d84c5251627b90b232513a20087c
Øivind Heyerdahl Elfving <othorval@soprasteria.com>
2017-12-12 21:19:36 +0100

    config file

--- config_hidden_example.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# If you are a "Hacker" scanning GitHub for passwords, this is not your golden ticket. This is a password/token to be able to register on tiny personal site. Good luck further.
-SitePassword = u'SomePassword'
