165a98574322e5caa3afc8b6f76d15df351e5aec
André S. Hansen <andre.ok@online.no>
2018-02-06 05:40:35 +0100

    Minor ps scripts changes

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,4 +1,6 @@
+Read-Host -Prompt "You are about to deploy - press any key to continue"
+
 "Deploying to google cloud"
 & gcloud app deploy ../app.yaml --project game-night-stats --quiet
 
-Read-Host -Prompt "Finished deployment - press enter to exit"
+Read-Host -Prompt "Finished deployment - press any key to exit"
