7969c0fcf385aad71eace0f9d92c837c1291b9f9
André S. Hansen <andre.ok@online.no>
2018-02-11 03:06:09 +0100

    Stats vote behavior with gn count adjusted numbers, and some table styling. #41

--- api/actions.py
+++ api/actions.py
@@ -34,27 +34,33 @@ class StatsHandler(webapp2.RequestHandler):
             # Stats - Vote Behavior
             vote_count = len(gn._votes) * 1.0
             avg_vote = {
-                'sum': sum([vote.nonweighed_sum() for vote in gn._votes]) / vote_count,
                 'appetizer': sum([vote.appertizer_int() for vote in gn._votes]) / vote_count, 
                 'main_course': sum([vote.main_course_int() for vote in gn._votes]) / vote_count,  
                 'dessert': sum([vote.dessert_int() for vote in gn._votes]) / vote_count,  
                 'game': sum([vote.game_int() for vote in gn._votes]) / vote_count, 
+                'sum': sum([vote.nonweighed_sum() for vote in gn._votes]) / vote_count,
             }
 
             for vote in gn._votes:
                 if vote.voter not in behaviors:
-                    behaviors[vote.voter] = { 'sum': 0, 'appetizer': 0, 'main_course': 0, 'dessert': 0, 'game': 0 }
-
-                behaviors[vote.voter]['sum'] += vote.nonweighed_sum() - avg_vote['sum']
+                    behaviors[vote.voter] = { 'count': 0, 'sum': 0, 'appetizer': 0, 'main_course': 0, 'dessert': 0, 'game': 0 }
+                
+                behaviors[vote.voter]['count'] += 1
                 behaviors[vote.voter]['appetizer'] += vote.appertizer_int() - avg_vote['appetizer']
                 behaviors[vote.voter]['main_course'] += vote.main_course_int() - avg_vote['main_course']
                 behaviors[vote.voter]['dessert'] += vote.dessert_int() - avg_vote['dessert']
                 behaviors[vote.voter]['game'] += vote.game_int() - avg_vote['game']
+                behaviors[vote.voter]['sum'] += vote.nonweighed_sum() - avg_vote['sum']
         
         # Convert to array as its better for the client
         behaviors_arr = []
         for person, behavior in behaviors.iteritems():
             behavior['person'] = person
+            behavior['appetizer'] = behavior['appetizer'] / behavior['count']
+            behavior['main_course'] = behavior['main_course'] / behavior['count']
+            behavior['dessert'] = behavior['dessert'] / behavior['count']
+            behavior['game'] = behavior['game'] / behavior['count']
+            behavior['sum'] = behavior['sum'] / behavior['count']
             behaviors_arr.append(behavior)
 
         set_json_response(self.response, {'behaviors': behaviors_arr})
