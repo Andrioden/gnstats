c963d6995364c8e56fd0bdeff7200cb893e1e81b
André S. Hansen <andre.ok@online.no>
2020-09-08 18:46:12 +0200

    Updated run_dev_server ps script with enable console flag true

--- scripts/run_dev_server.ps1
+++ scripts/run_dev_server.ps1
@@ -1,3 +1,5 @@
-$DevAppserverPath = "C:\Users\andre\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin\dev_appserver.py"
+$DevAppserverPath = "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\dev_appserver.py"
 
-& python $DevAppserverPath --host=0.0.0.0 --enable_host_checking=false "..\app.yaml"
+& python $DevAppserverPath --host=0.0.0.0 --enable_host_checking=false --enable_console=true "..\app.yaml"
+
+Read-Host -Prompt "Finished run - press any key to exit"
