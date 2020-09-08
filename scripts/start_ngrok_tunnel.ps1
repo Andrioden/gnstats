1fcfce002a7173034fa8fb5be098cda868441ad5
André S. Hansen <andre.ok@online.no>
2020-09-08 18:45:23 +0200

    Moved nogrok ps script to scripts folder

--- scripts/start_ngrok_tunnel.ps1
+++ scripts/start_ngrok_tunnel.ps1
@@ -1,6 +1,6 @@
 $ngrok_path = "C:\bin\ngrok.exe"
 
 "Starting tunnel using $ngrok_path"
-& $ngrok_path http 11080 -host-header="localhost:11080"
+& $ngrok_path http 8080 -host-header="localhost:8080"
 
 Read-Host -Prompt "Tunnel stopped - press enter to exit"
