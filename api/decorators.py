48cbc0517d6d107895a80742d17429498566a215
André S. Hansen <andre.ok@online.no>
2022-10-04 23:29:46 +0200

    commented out unused decorators

--- api/decorators.py
+++ api/decorators.py
@@ -6,7 +6,7 @@ from google.cloud.ndb import get_context
 
 from .utils import *
 
-DECORATOR_NO_REQUEST_ATTRIBUTE_HELP_TEXT = "You have used the decorator on something that does not have a request attribute. Have you used it on the class instead of the class get/post function?"
+# DECORATOR_NO_REQUEST_ATTRIBUTE_HELP_TEXT = "You have used the decorator on something that does not have a request attribute. Have you used it on the class instead of the class get/post function?"
 
 
 def ensure_db_context(func):
@@ -34,32 +34,32 @@ def ensure_db_context(func):
 #     return wrapper
 
 
-def require_admin(func):
-    @wraps(func)
-    def wrapper(request_handler, *args):
-        if not hasattr(request_handler, "request"):
-            raise Exception(DECORATOR_NO_REQUEST_ATTRIBUTE_HELP_TEXT)
+# def require_admin(func):
+#     @wraps(func)
+#     def wrapper(request_handler, *args):
+#         if not hasattr(request_handler, "request"):
+#             raise Exception(DECORATOR_NO_REQUEST_ATTRIBUTE_HELP_TEXT)
+#
+#         if not validate_admin(request_handler.response):
+#             return None
+#         else:
+#             return func(request_handler, *args)
+#
+#     return wrapper
 
-        if not validate_admin(request_handler.response):
-            return None
-        else:
-            return func(request_handler, *args)
 
-    return wrapper
-
-
-def require_request_data(required_data):
-    def actual_decorator(func):
-        @wraps(func)
-        def wrapper(request_handler, *args):
-            if not hasattr(request_handler, "request"):
-                raise Exception(DECORATOR_NO_REQUEST_ATTRIBUTE_HELP_TEXT)
-
-            if not validate_request_data(request_handler, required_data):
-                return None
-            else:
-                return func(request_handler, *args)
-
-        return wrapper
-
-    return actual_decorator
+# def require_request_data(required_data):
+#     def actual_decorator(func):
+#         @wraps(func)
+#         def wrapper(request_handler, *args):
+#             if not hasattr(request_handler, "request"):
+#                 raise Exception(DECORATOR_NO_REQUEST_ATTRIBUTE_HELP_TEXT)
+#
+#             if not validate_request_data(request_handler, required_data):
+#                 return None
+#             else:
+#                 return func(request_handler, *args)
+#
+#         return wrapper
+#
+#     return actual_decorator
