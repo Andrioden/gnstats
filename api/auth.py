51dd517250e5cbdd9e48cf9c699a0424ecc75696
André S. Hansen <andre.ok@online.no>
2022-10-06 01:15:13 +0200

    User flow documented in auth.py

--- api/auth.py
+++ api/auth.py
@@ -1,3 +1,14 @@
+"""
+The user auth flow:
+1. The user calls our /login/, a login url is created
+2. User redirected to a Google hosted login page
+3. User logs inn to their Google account
+4. Google redirects user back to our /callback/
+5. We call Google service and validates callback data
+6. We store a validated data in the user session
+7. Starlette middleware validate on every request user session based on crypto magic and SECRET_KEY
+"""
+
 from authlib.integrations.starlette_client import StarletteOAuth2App
 from fastapi import APIRouter, Depends, Request
 from starlette.responses import RedirectResponse
