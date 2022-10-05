f6ce34dbe61f7d883861c137b0691d86bfc89080
André S. Hansen <andre.ok@online.no>
2022-10-05 21:11:35 +0200

    mypy disallow_subclassing_any = true

--- models/db/base.py
+++ models/db/base.py
@@ -1,7 +1,7 @@
 from google.cloud.ndb import Model
 
 
-class DbModelBase(Model):
+class DbModelBase(Model):  # type: ignore[misc]
     @property
     def id(self) -> int:
         return self.key.id()  # type: ignore[union-attr]
